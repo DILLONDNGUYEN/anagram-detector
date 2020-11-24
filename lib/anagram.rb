d0f80cb1654142d1a827e9a2419818a509a734e8
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-11-23 18:48:38 -0800

    commit tree fb7cb2f003e00070dcc7f1e501ec4e7d4ffa177b

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :analyze
+  
+  def initialize(analyze)
+    @analyze = analyze 
+  end 
+  
+  def match(anagram) 
+    puts "Must find #{self.analyze} inside the following string: #{anagram}"
+    anagram.find_all do |word| 
+      if word.split("").sort == self.analyze.split("").sort 
+        word 
+      end
+
+    end
+
+  end
+  
+end 
